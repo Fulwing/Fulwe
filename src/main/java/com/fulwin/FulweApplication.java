ecb8433ca49b5f6bd365a34cc5396148e0246206
Daniel Yan <danielyanzhengyuhan@gmail.com>
2023-09-10 18:17:25 -0400

    start project

---
+++ src/main/java/com/fulwin/FulweApplication.java
@@ -0,0 +1,13 @@
+package com.fulwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulweApplication.class, args);
+	}
+
+}
