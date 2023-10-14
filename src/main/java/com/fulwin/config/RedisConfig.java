75488a8ac072a5c175f26dc6d4408a42b0d92499
Daniel Yan <danielyanzhengyuhan@gmail.com>
2023-10-14 02:13:37 -0400

    added redis

--- src/main/java/com/fulwin/config/RedisConfig.java
+++ src/main/java/com/fulwin/config/RedisConfig.java
@@ -1,12 +1,14 @@
 package com.fulwin.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.net.UnknownHostException;
 
+@Configuration
 public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
